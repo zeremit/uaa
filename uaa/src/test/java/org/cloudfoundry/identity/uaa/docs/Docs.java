46780aa89466606ff0ea52ba2d63b00f13de516f
Paul Warren <paul.warren@emc.com>
2015-12-09 11:06:15 -0800

    Add ability for tests to configure the mockmvc client
    [#109725544] https://www.pivotaltracker.com/story/show/109725544
    
    Signed-off-by: Jeremy Coffield <jcoffield@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/docs/Docs.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/docs/Docs.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentation;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.ConfigurableMockMvcBuilder;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
