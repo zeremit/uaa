5fb85b7d68f071f34f08d1b6491ba0a81f687dbc
Priyata Agrawal <pagrawal@pivotal.io>
2016-04-19 13:33:10 -0700

    Display default values in snippet output
    [#117363941] https://www.pivotaltracker.com/story/show/117363941
    
    Signed-off-by: Jeremy Coffield <jcoffield@pivotal.io>

--- uaa/src/test/java/org/cloudfoundry/identity/uaa/test/SnippetUtils.java
+++ uaa/src/test/java/org/cloudfoundry/identity/uaa/test/SnippetUtils.java
@@ -1,10 +1,12 @@
 package org.cloudfoundry.identity.uaa.test;
 
+import org.cloudfoundry.identity.uaa.util.JsonUtils;
 import org.springframework.restdocs.payload.FieldDescriptor;
 import org.springframework.restdocs.request.ParameterDescriptor;
 import org.springframework.restdocs.snippet.Attributes;
 
 import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.util.StringUtils.hasText;
 
 public final class SnippetUtils {
     private SnippetUtils() {}
@@ -26,11 +28,8 @@ public final class SnippetUtils {
             return attributes(key("constraints").value("Required"));
         }
 
-        public ParameterDescriptor optional(Object defaultValue) {
-            if(defaultValue == null) {
-                defaultValue = "";
-            }
-            Attributes.Attribute[] attrs = new Attributes.Attribute[] { key("constraints").value("Optional"), key("default").value(defaultValue) };
+        public ParameterDescriptor optional(String defaultValue) {
+            Attributes.Attribute[] attrs = new Attributes.Attribute[] {key("constraints").value(hasText(defaultValue) ? "Optional (defaults to `" + defaultValue + "`)" : "Optional")};
             return attributes(attrs);
         }
     }
@@ -46,11 +45,16 @@ public final class SnippetUtils {
         }
 
         public FieldDescriptor optional(Object defaultValue) {
+            String defaultValueText;
             if(defaultValue == null) {
-                defaultValue = "";
+                defaultValueText = "";
+            } else {
+                defaultValueText = JsonUtils.writeValueAsString(defaultValue);
             }
-            Attributes.Attribute[] attrs = new Attributes.Attribute[] { key("constraints").value("Optional"), key("default").value(defaultValue) };
+
+            Attributes.Attribute[] attrs = new Attributes.Attribute[] {key("constraints").value(hasText(defaultValueText) ? "Optional (defaults to `" + defaultValueText + "`)" : "Optional")};
             return attributes(attrs);
         }
     }
+
 }
